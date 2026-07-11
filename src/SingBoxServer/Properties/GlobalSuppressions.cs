8f5c089a532a5632c9e46951e61f5272d04e03ba
megapro17 <megapro17@gmail.com>
2026-07-11 04:56:46 +0300

    chore: точечное подавление предупреждений CA1812 для DI-сервисов

---
+++ src/SingBoxServer/Properties/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "Instantiated by DI", Scope = "type", Target = "~T:SingBoxServer.Core.PlatformPath")]
+[assembly: SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "Instantiated by DI", Scope = "type", Target = "~T:SingBoxServer.Core.PlatformPath.Setup")]
+[assembly: SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "Instantiated by DI", Scope = "type", Target = "~T:SingBoxServer.Services.ConfigurationService")]
+[assembly: SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "Instantiated by DI", Scope = "type", Target = "~T:SingBoxServer.Services.Generators.SingBox.SingBoxGenerator")]
+[assembly: SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "Instantiated by DI", Scope = "type", Target = "~T:SingBoxServer.Services.Subscriptions.LocalFileCache")]
+[assembly: SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "Instantiated by DI", Scope = "type", Target = "~T:SingBoxServer.Services.Subscriptions.RemoteSubscriptionCache")]
+[assembly: SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "Instantiated by DI", Scope = "type", Target = "~T:SingBoxServer.Services.Subscriptions.SubscriptionLoader")]
